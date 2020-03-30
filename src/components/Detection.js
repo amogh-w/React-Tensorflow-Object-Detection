8bf8035132e35787ecf1ae8cc046ec81bc1d888f
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-03-30 18:52:51 +0530

    Finished Drawing on Canvas

--- src/components/Detection.js
+++ src/components/Detection.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Typography, Button } from "@material-ui/core";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
+import Prediction from "./Prediction";
 
 const Detection = () => {
   const [model, setModel] = useState(null);
   const [isModelLoaded, setIsModelLoaded] = useState(false);
   const [imageSrc, setImageSrc] = useState(null);
+  const [predictions, setPredictions] = useState(null);
 
   useEffect(() => {
     const loadModel = async () => {
@@ -57,11 +59,16 @@ const Detection = () => {
       <WebcamCapture />
       <Button
         onClick={() =>
-          console.log(model.detect(document.getElementById("webcamFeed")))
+          model
+            .detect(document.getElementById("webcamFeed"))
+            .then(predictions => {
+              setPredictions(predictions);
+            })
         }
       >
         Predict
       </Button>
+      <Prediction imageSrc={imageSrc} predictions={predictions} />
     </React.Fragment>
   );
 };
